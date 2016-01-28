cdc884573eedc45f80aaecb2d363a5d04a8747b2
Wesley <webredhacker@163.com>
2016-01-28 18:31:54 +0800

    调整异常捕获方式

--- Wesley.Component.Captcha/Decoder.cs
+++ Wesley.Component.Captcha/Decoder.cs
@@ -43,32 +43,25 @@ namespace Wesley.Component.Captcha
 
         public async Task Decode(string filePath)
         {
-            try
+            await Task.Run(() =>
             {
-                if (string.IsNullOrWhiteSpace(filePath)) throw new Exception("图片文件路径不能为空！");
-                if (!File.Exists(filePath)) throw new Exception("验证码图片文件（" + filePath + "）不存在！");
-                if (this._strategy == null) throw new Exception("不能实例化此平台（" + this._platform + "）的识别策略！");
-                if (OnStart != null) this.OnStart(this, new OnStartedEventArgs(filePath));
-                await Task.Run(() =>
+                try
                 {
-                    try
-                    {
-                        var startTime = DateTime.Now;
-                        var code = this._strategy.Recognize(filePath);
-                        var threadId = Thread.CurrentThread.ManagedThreadId;
-                        var millisecond = DateTime.Now.Subtract(startTime).TotalMilliseconds;
-                        if (OnCompleted != null) this.OnCompleted(this, new OnCompletedEventArgs(code, millisecond, filePath, threadId));
-                    }
-                    catch (Exception ex)
-                    {
-                        if (OnError != null) this.OnError(this, new OnErrorEventArgs(filePath, ex));
-                    }
-                });
-            }
-            catch (Exception ex)
-            {
-                if (OnError != null) this.OnError(this, new OnErrorEventArgs(filePath, ex));
-            }
+                    if (string.IsNullOrWhiteSpace(filePath)) throw new Exception("图片文件路径不能为空！");
+                    if (!File.Exists(filePath)) throw new Exception("验证码图片文件（" + filePath + "）不存在！");
+                    if (this._strategy == null) throw new Exception("不能实例化（" + this._platform + "）平台的识别策略！");
+                    if (OnStart != null) this.OnStart(this, new OnStartedEventArgs(filePath));
+                    var startTime = DateTime.Now;
+                    var code = this._strategy.Recognize(filePath);
+                    var threadId = Thread.CurrentThread.ManagedThreadId;
+                    var millisecond = DateTime.Now.Subtract(startTime).TotalMilliseconds;
+                    if (OnCompleted != null) this.OnCompleted(this, new OnCompletedEventArgs(code, millisecond, filePath, threadId));
+                }
+                catch (Exception ex)
+                {
+                    if (OnError != null) this.OnError(this, new OnErrorEventArgs(filePath, ex));
+                }
+            });
         }
     }
 }
