e4e6cf08bf1e07bab0dcab03db696ae35a30e7de
Wesley <276679490@qq.com>
2016-01-28 14:52:25 +0800

    更改若快打码平台至Json数据格式

--- Wesley.Component.Captcha/Strategies/RuoKuai/RuoKuaiStrategy.cs
+++ Wesley.Component.Captcha/Strategies/RuoKuai/RuoKuaiStrategy.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
+using Newtonsoft.Json;
 
 namespace Wesley.Component.Captcha.Strategies.RuoKuai
 {
@@ -32,38 +33,19 @@ namespace Wesley.Component.Captcha.Strategies.RuoKuai
             if (string.IsNullOrWhiteSpace(this.Account.SoftKey)) throw new Exception("请输入平台软件Key！");
             if (string.IsNullOrWhiteSpace(this.Account.UserName)) throw new Exception("请输入平台普通用户账号！");
             if (string.IsNullOrWhiteSpace(this.Account.Password)) throw new Exception("请输入平台密码！");
-
-            string result;
             var param = new Dictionary<object, object>
-                {
-                    {"username",Account.UserName},
-                    {"password",Account.Password},
-                    {"typeid",Account.TypeId},
-                    {"timeout","90"},
-                    {"softid",Account.SoftId},
-                    {"softkey",Account.SoftKey}
-                };
-
+            {
+                {"username",Account.UserName},
+                {"password",Account.Password},
+                {"typeid",Account.TypeId},
+                {"timeout","90"},
+                {"softid",Account.SoftId},
+                {"softkey",Account.SoftKey}
+            };
             var data = File.ReadAllBytes(filePath);
-            var httpResult = RuoKuaiHttp.Post("http://api.ruokuai.com/create.xml", param, data);
-            var xmlDoc = new XmlDocument();
-            xmlDoc.LoadXml(httpResult);
-            var idNode = xmlDoc.SelectSingleNode("Root/Id");
-            var resultNode = xmlDoc.SelectSingleNode("Root/Result");
-            var errorNode = xmlDoc.SelectSingleNode("Root/Error");
-            if (resultNode != null && idNode != null)
-            {
-                result = resultNode.InnerText;
-            }
-            else if (errorNode != null)
-            {
-                throw new Exception(errorNode.InnerText);
-            }
-            else
-            {
-                throw new Exception("未知问题。");
-            }
-            return result;
+            var response = RuoKuaiHttp.Post("http://api.ruokuai.com/create.json", param, data);
+            dynamic json = JsonConvert.DeserializeObject(response);
+            return json.Result;
         }
     }
 }
